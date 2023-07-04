788b8dfb09a553c2054f2b372012f03eccf6e5da
Christos Tsatsampas <xristostsatsabas@gmail.com>
2023-07-04 23:40:08 +0300

    broken commit

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -21,4 +21,10 @@ public class GreetingResource {
         return "Hello from RESTEasy Reactive";
     }
     
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from RESTEasy Reactive";
+    }
+    
 }
